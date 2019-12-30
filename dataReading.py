378cf5430daacb4290c21cc2ae0d505b302eeb05
root <jimkak927@hotmail.com>
2019-12-30 12:25:14 +0200

    !commit

--- dataReading.py
+++ dataReading.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 from scipy.stats import ks_2samp,chisquare,chi2_contingency
 import math
+import pyearth as sp
+import matplotlib
 import random
 import csv
 import datetime
@@ -28,6 +30,359 @@ class BaseSeriesReader:
 
         return np.nan_to_num(X.astype(float)),np.nan_to_num(Y.astype(float)) , np.nan_to_num(W.astype(float))
 
+    def readNewDataset(self):
+
+        ####STATSTICAL ANALYSIS PENELOPE
+
+        data = pd.read_csv('./PENELOPE_mapped.csv')
+        data1 = pd.read_csv('./PENELOPE_mapped_1.csv')
+        dtNew = data.values.astype(float)
+        dtNew1 = data1.values.astype(float)
+        dataNew = np.concatenate([ dtNew, dtNew1 ])
+
+        ####################
+        ####################
+        ####################
+        dataV = pd.read_csv('./data/PENELOPE_1-28_02_19.csv')
+        dataV=dataV.drop([ 't_stamp', 'vessel status' ], axis=1)
+        dtNew = dataV.values.astype(float)
+
+        windSpeedVmapped = [ ]
+        windDirsVmapped = [ ]
+        draftsVmapped = [ ]
+        with open('./windSpeedMapped.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                windSpeedVmapped.append(float(row[ 0 ]))
+        with open('./windDirMapped.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                windDirsVmapped.append(float(row[ 0 ]))
+
+        with open('./draftsMapped.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                draftsVmapped.append(float(row[ 0 ]))
+
+
+
+        newMappedData = np.array(
+            np.append(dtNew[ :, : ], np.asmatrix([ np.array(windSpeedVmapped), np.array(windDirsVmapped) ]).T,
+                      axis=1)).astype(float)
+        Vcourse = newMappedData[ :, 2 ]
+        Vstw = newMappedData[ :, 1 ]
+        windDir = newMappedData[ :, 27 ]
+        windSpeed = newMappedData[ :, 26 ]
+        ###projection of vector wind speed direction vector into vessel speed direction orthogonal system of coordinates
+        ##convert vessel speed (knots) to m/s
+        relWindSpeeds = [ ]
+        relWindDirs = [ ]
+        for i in range(0, len(Vcourse)):
+            x = np.array([ Vstw[ i ] * 0.514, Vcourse[ i ] ])
+            y = np.array([ windSpeed[ i ], windDir[ i ] ])
+            ##convert cartesian coordinates to polar
+            x = np.array([ x[ 0 ] * np.cos(x[ 1 ]), x[ 0 ] * np.sin(x[ 1 ]) ])
+            y = np.array([ y[ 0 ] * np.cos(y[ 1 ]), y[ 0 ] * np.sin(y[ 1 ]) ])
+
+            x_norm = np.sqrt(sum(x ** 2))
+
+            # Apply the formula as mentioned above
+            # for projecting a vector onto the orthogonal vector n
+            # find dot product using np.dot()
+            proj_of_y_on_x = (np.dot(y, x) / x_norm ** 2) * x
+
+            vecproj = np.dot(y, x) / np.dot(y, y) * y
+            relWindSpeed = proj_of_y_on_x[ 0 ] + windSpeed[ i ]
+            if relWindSpeed > 40:
+                d = 0
+            relDir = windDir[ i ] - Vcourse[ i ]
+            relDir += 360 if relDir < 0 else relDir
+            relWindDirs.append(relDir)
+            relWindSpeeds.append(relWindSpeed)
+        newMappedData = np.array(
+            np.append(dtNew[ :, : ],
+                      np.asmatrix([ np.array(relWindSpeeds), np.array(relWindDirs), np.array(draftsVmapped) ]).T,
+                      axis=1)).astype(float)
+
+        #####################
+        ############################MAP TELEGRAM DATES FOR DRAFT
+
+
+
+        x = 0
+
+    def readExtractNewDataset(self):
+
+        ####################
+        ####################
+        ####################
+        dataV = pd.read_csv('./data/PENELOPE_1-30_11_19.csv')
+        #dataV=dataV.drop([ 't_stamp', 'vessel status' ], axis=1)
+        dtNew = dataV.values#.astype(float)
+
+        windSpeedVmapped = [ ]
+        windDirsVmapped = [ ]
+
+        dateTimesV = [ ]
+        for row in dtNew[ :, 0 ]:
+            date = row.split(" ")[ 0 ]
+            hhMMss = row.split(" ")[ 1 ]
+            newDate = date + " " + ":".join(hhMMss.split(":")[ 0:2 ])
+            dt = datetime.datetime.strptime(newDate, '%Y-%m-%d %H:%M')
+            dateTimesV.append(dt)
+
+        data = pd.read_excel('./Penelope TrackReport.xls')
+        WdtNew = data.values[ 0:, : ]
+        dateTimesW = [ ]
+        ws = [ ]
+
+        for i in range(0, len(WdtNew[ :, 1 ])):
+            date = WdtNew[ i, 1 ].split(" ")[ 0 ]
+            month = date.split('.')[ 1 ]
+            day = date.split('.')[ 0 ]
+            year = date.split('.')[ 2 ]
+
+            hhMMss = WdtNew[ i, 1 ].split(" ")[ 1 ]
+            newDate = year + "-" + month + "-" + day + " " + ":".join(hhMMss.split(":")[ 0:2 ])
+            dt = datetime.datetime.strptime(newDate, '%Y-%m-%d %H:%M')
+            if WdtNew[ i, 7 ] != 'No data' and WdtNew[ i, 8 ] != 'No data':
+                dateTimesW.append(dt)
+
+        for i in range(0, len(dateTimesV)):
+
+            dtDiff = {}
+            datesWs = [ ]
+
+            date = str(dateTimesV[ i ]).split(" ")[ 0 ]
+            month = date.split('-')[ 1 ]
+            day = date.split('-')[ 2 ]
+            year = date.split('-')[ 0 ]
+            dt = dateTimesV[ i ]
+
+            filteredDTWs = [ d for d in dateTimesW if month == str(d).split(' ')[ 0 ].split('-')[ 1 ] and day ==
+                             str(d).split(' ')[ 0 ].split('-')[ 2 ] and year == str(d).split(' ')[ 0 ].split('-')[ 0 ] ]
+
+            for k in range(0, len(filteredDTWs)):
+                # dtDiff[filteredDTWs[k]]=abs((filteredDTWs[k]-dt).total_seconds() / 60.0)
+                date = str(filteredDTWs[ k ]).split(" ")[ 0 ]
+                month = date.split('-')[ 1 ]
+                day = date.split('-')[ 2 ]
+                year = date.split('-')[ 0 ]
+                hhMMss = str(filteredDTWs[ k ]).split(" ")[ 1 ]
+                newDate = day + "." + month + "." + year + " " + ":".join(hhMMss.split(":")[ 0:2 ])
+                datesWs.append(newDate)
+
+            # firstMin = min(dtDiff.items(), key=lambda x: x[1])
+            # del dtDiff[ firstMin[ 0 ] ]
+            # secMin = min(dtDiff.items(), key=lambda x: x[ 1 ] )
+
+            windSpeeds = [ ]
+            windDirs = [ ]
+            for date in datesWs:
+                ws = np.array(data.loc[ data[ 'Date' ] == date + " Z" ].values)[ 0 ][ 7 ]
+                wd = np.array(data.loc[ data[ 'Date' ] == date + " Z" ].values)[ 0 ][ 8 ]
+                if ws != 'No data':
+                    windSpeeds.append(float(ws.split('m')[ 0 ]))
+                if wd != 'No data':
+                    windDirs.append(float(wd[ :len(wd) - 1 ]))
+
+            # df = pd.DataFrame([ws1,None,ws2], index=[firstMin,dt,secMin])
+            # resampled = df.resample('S')
+            # df.head()
+            # interp = df.interpolate()
+            # print(interp.head())
+            # windSpeedVmapped.append(interp.values[1])
+            from scipy.interpolate import interp1d
+
+            y = np.array(windSpeeds)
+            x = matplotlib.dates.date2num(filteredDTWs)
+            f = sp.Earth(max_degree=2)
+            f.fit(x.reshape(-1, 1), y.reshape(-1, 1))
+            pred = f.predict(np.array(matplotlib.dates.date2num(dt)).reshape(-1, 1))
+            windSpeedVmapped.append(pred)
+
+            y = np.array(windDirs)
+            x = matplotlib.dates.date2num(filteredDTWs)
+            f = sp.Earth(max_degree=2)
+
+            f.fit(x.reshape(-1, 1), y.reshape(-1, 1))
+            pred = f.predict(np.array(matplotlib.dates.date2num(dt)).reshape(-1, 1))
+            # f=interp1d(list(x), list(y), kind='cubic')
+            windDirsVmapped.append(pred)
+
+        ######DRAFT
+        dateTimesV = [ ]
+        for row in dtNew[ :, 0 ]:
+            date = row.split(" ")[ 0 ]
+            hhMMss = row.split(" ")[ 1 ]
+            newDate = date + " " + ":".join(hhMMss.split(":")[ 0:2 ])
+            dt = datetime.datetime.strptime(newDate, '%Y-%m-%d %H:%M')
+            dateTimesV.append(dt)
+
+        data = pd.read_csv('./export telegram.csv', sep='\t')
+        DdtNew = data.values[ 0:, : ]
+        dateTimesW = [ ]
+        ws = [ ]
+
+        for i in range(0, len(DdtNew[ :, 1 ])):
+            if DdtNew[ i, 0 ] == 'T003':
+                date = DdtNew[ i, 1 ].split(" ")[ 0 ]
+                month = date.split('/')[ 1 ]
+                day = date.split('/')[ 0 ]
+                year = date.split('/')[ 2 ]
+
+                hhMMss = DdtNew[ i, 1 ].split(" ")[ 1 ]
+                newDate = year + "-" + month + "-" + day + " " + ":".join(hhMMss.split(":")[ 0:2 ])
+                dt = datetime.datetime.strptime(newDate, '%Y-%m-%d %H:%M')
+                # if DdtNew[ i, 7 ] != 'No data' and DdtNew[ i, 8 ] != 'No data':
+                dateTimesW.append(dt)
+
+        draftsVmapped = [ ]
+
+        for i in range(0, len(dateTimesV)):
+
+            datesWs = [ ]
+
+            date = str(dateTimesV[ i ]).split(" ")[ 0 ]
+            month = date.split('-')[ 1 ]
+
+            day = date.split('-')[ 2 ]
+
+            year = date.split('-')[ 0 ]
+            dt = dateTimesV[ i ]
+
+            filteredDTWs = [ d for d in dateTimesW if month == str(d).split(' ')[ 0 ].split('-')[ 1 ] and day ==
+                             str(d).split(' ')[ 0 ].split('-')[ 2 ] and year == str(d).split(' ')[ 0 ].split('-')[ 0 ] ]
+
+            for k in range(0, len(filteredDTWs)):
+                # dtDiff[filteredDTWs[k]]=abs((filteredDTWs[k]-dt).total_seconds() / 60.0)
+                date = str(filteredDTWs[ k ]).split(" ")[ 0 ]
+                month = date.split('-')[ 1 ]
+                month = month[ 1 ] if month[ 0 ] == '0' else month
+                day = date.split('-')[ 2 ]
+                day = day[ 1 ] if day[ 0 ] == '0' else day
+                year = date.split('-')[ 0 ]
+                hhMMss = str(filteredDTWs[ k ]).split(" ")[ 1 ]
+                # newDate = month + "/" + day + "/" + year + " " + ":".join(hhMMss.split(":")[ 0:2 ])
+                newDate = day + "/" + month + "/" + year + " " + ":".join(hhMMss.split(":"))
+                datesWs.append(newDate)
+
+            # firstMin = min(dtDiff.items(), key=lambda x: x[1])
+            # del dtDiff[ firstMin[ 0 ] ]
+            # secMin = min(dtDiff.items(), key=lambda x: x[ 1 ] )
+
+            drafts = [ ]
+            for date in datesWs:
+                drftAFT = np.array(data.loc[ data[ 'telegram_date' ] == date ].values)[ 0 ][ 27 ]
+                drftFORE = np.array(data.loc[ data[ 'telegram_date' ] == date ].values)[ 0 ][ 28 ]
+
+                drftAFT = float(drftAFT.split(',')[ 0 ] + "." + drftAFT.split(',')[ 1 ]) if drftAFT.__contains__(
+                    ',') else float(drftAFT)
+                drftFORE = float(drftFORE.split(',')[ 0 ] + "." + drftFORE.split(',')[ 1 ]) if drftFORE.__contains__(
+                    ',') else float(drftFORE)
+
+                drft = (drftAFT + drftFORE) / 2
+                drafts.append(float(drft))
+
+            y = np.array(drafts)
+            x = matplotlib.dates.date2num(filteredDTWs)
+            f = sp.Earth()
+            f.fit(x.reshape(-1, 1), y.reshape(-1, 1))
+            pred = f.predict(np.array(matplotlib.dates.date2num(dt)).reshape(-1, 1))
+            draftsVmapped.append(pred)
+
+        ##########
+        #pred = f(matplotlib.dates.date2num(dt))
+        #data = pd.read_excel('./Penelope TrackReport.xls')
+        #WdtNew = data.values[ 0:, : ]
+            #print(pred)
+        with open('./windSpeedMapped.csv', mode='w') as dataw:
+            for k in range(0, len(windSpeedVmapped)):
+                data_writer = csv.writer(dataw, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                data_writer.writerow([ windSpeedVmapped[ k ][ 0 ] ])
+        with open('./windDirMapped.csv', mode='w') as dataw:
+            for k in range(0, len(windDirsVmapped)):
+                data_writer = csv.writer(dataw, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                data_writer.writerow([ windDirsVmapped[ k ][ 0 ] ])
+
+        with open('./draftsMapped.csv', mode='w') as dataw:
+            for k in range(0, len(draftsVmapped)):
+                data_writer = csv.writer(dataw, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                data_writer.writerow([ draftsVmapped[ k ][ 0 ] ])
+
+        windSpeedVmapped = [ ]
+        windDirsVmapped = [ ]
+        draftsVmapped=[]
+        with open('./windSpeedMapped.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                windSpeedVmapped.append(float(row[ 0 ]))
+        with open('./windDirMapped.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                windDirsVmapped.append(float(row[ 0 ]))
+
+        with open('./draftsMapped.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                draftsVmapped.append(float(row[ 0 ]))
+
+
+
+
+        dataV = pd.read_csv('./data/PENELOPE_1-30_11_19.csv')
+        dataV=dataV.drop([ 't_stamp', 'vessel status' ], axis=1)
+        dtNew = dataV.values.astype(float)
+
+        newMappedData = np.array(
+            np.append(dtNew[ :, : ], np.asmatrix([ np.array(windSpeedVmapped), np.array(windDirsVmapped) ]).T,
+                      axis=1)).astype(float)
+        Vcourse = newMappedData[ :, 2 ]
+        Vstw = newMappedData[ :, 1 ]
+        windDir = newMappedData[ :, 27 ]
+        windSpeed = newMappedData[ :, 26 ]
+        ###projection of vector wind speed direction vector into vessel speed direction orthogonal system of coordinates
+        ##convert vessel speed (knots) to m/s
+        relWindSpeeds = [ ]
+        relWindDirs = [ ]
+        for i in range(0, len(Vcourse)):
+            x = np.array([ Vstw[ i ] * 0.514, Vcourse[ i ] ])
+            y = np.array([ windSpeed[ i ], windDir[ i ] ])
+            ##convert cartesian coordinates to polar
+            x = np.array([x[0] * np.cos(x[1]),x[0]* np.sin(x[1])])
+            y = np.array([ y[ 0 ] * np.cos(y[ 1 ]), y[ 0 ] * np.sin(y[ 1 ]) ])
+
+            x_norm = np.sqrt(sum(x ** 2))
+
+            # Apply the formula as mentioned above
+            # for projecting a vector onto the orthogonal vector n
+            # find dot product using np.dot()
+            proj_of_y_on_x = (np.dot(y, x) / x_norm ** 2) *x
+
+            vecproj =  np.dot(y, x) / np.dot(y, y) * y
+            relWindSpeed = proj_of_y_on_x[ 0 ] + windSpeed[ i ]
+            if relWindSpeed > 40:
+                d=0
+            relDir = windDir[ i ] - Vcourse[ i ]
+            relDir += 360 if relDir < 0 else relDir
+            relWindDirs.append(relDir)
+            relWindSpeeds.append(relWindSpeed)
+        newMappedData = np.array(
+            np.append(dtNew[ :, : ],
+                      np.asmatrix([ np.array(relWindSpeeds), np.array(relWindDirs), np.array(draftsVmapped) ]).T,
+                      axis=1)).astype(float)
+
+        #####################
+        ############################MAP TELEGRAM DATES FOR DRAFT
+
+        ####STATSTICAL ANALYSIS PENELOPE
+        with open('./PENELOPE_mapped_1.csv', mode='w') as dataw:
+            data_writer = csv.writer(dataw, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            data_writer.writerow([ 'STW',  'WindSpeed', 'WindAngle', 'Draft'
+                                     , 'M/E FOC (MT/days)' ])
+            for k in range(0, len(newMappedData)):
+                data_writer.writerow([ newMappedData[ k ][ 1 ],newMappedData[ k ][ 26 ],newMappedData[ k ][ 27 ],newMappedData[ k ][ 28 ],newMappedData[ k ][ 9 ]  ])
+        x = 0
 
     def readLarosDAta(self,dtFrom,dtTo):
         UNK = {"data": []}
@@ -843,6 +1198,7 @@ class BaseSeriesReader:
         #tw19DrftB4 = np.concatenate(drftStw19)
 
         x=0
+
     def readLarosDataFromCsvNew(self, data):
         # Load file
         from sklearn import preprocessing
