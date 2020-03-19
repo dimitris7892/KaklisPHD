23f41cba6b9eb3081726ef91e78bb023e9fb6ac9
jimkak927@hotmail.com <jimkak927@hotmail.com>
2020-03-19 19:33:19 -0400

    !

--- runPipeline1.py
+++ runPipeline1.py
@@ -49,7 +49,7 @@ def main():
 
     #DANreader.GenericParserForDataExtraction('LAROS','MARMARAS','MT_DELTA_MARIA')
     #DANreader.GenericParserForDataExtraction('LEMAG', 'MILLENIA', 'DOMINIA',driver='ORACLE',server='10.2.5.80',sid='OR12',usr='goldenport',password='goldenport',
-                                             rawData=[],telegrams=True,pathOfRawData='C:/Users/dkaklis/Desktop/danaos')
+                                             #rawData=[],telegrams=True,pathOfRawData='C:/Users/dkaklis/Desktop/danaos')
     #DANreader.readExtractNewDataset('MILLENIA','FANTASIA',';')
     #return
     #DANreader.ExtractLAROSDataset("",'2017-06-01 00:00:00','2019-10-09 15:10:00')
@@ -323,7 +323,7 @@ def initParameters():
     end = 17000
     startU = 30000
     endU = 31000
-    algs=['NNW']
+    algs=['NNW1']
     # ['SR','LR','RF','NN','NNW','TRI']
 
 
