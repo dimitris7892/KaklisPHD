5efe2729b98696f5935be74cf16203005aa90e20
dkaklis <jimkak927@hotmail.com>
2020-05-25 12:20:51 +0300

    !

--- Danaos_ML_Project/runPipeline1.py
+++ Danaos_ML_Project/runPipeline1.py
@@ -20,7 +20,7 @@ import  matplotlib.pyplot as plt
 import csv
 import tensorflow as tf
 from matplotlib import rc
-import latex
+#import latex
 #rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 #rc('text', usetex=True)
 def main():
@@ -158,7 +158,7 @@ def main():
     # unseensY.append(data[(k+kInit):(n+k+kInit), 7])
     # k = n * i + 10
 
-    trData = data[0:89999]
+    trData = data[0:1000]
     k=0
     kInit =0
     n=20000
@@ -316,7 +316,7 @@ def main():
                         modeler, None, None, None, partitionsX, None, subsetInd,'train')
 
                 print("Mean absolute error on seen data: %4.2f (+/- %4.2f standard error)" % (
-                meanError, sdError / sqrt(unseenY.shape[0])))
+                meanErrorTr, sdError / sqrt(unseenY.shape[0])))
                 print("Evaluating on seen data... Done.")
 
                 #Predict and evaluate on unseen data
